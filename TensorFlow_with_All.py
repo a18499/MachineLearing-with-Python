d104459530014cede13c6c0185b232f01a7d1db5
PeterHuang <a184999@gmail.com>
2017-06-15 17:14:54 +0800

    [New]

--- TensorFlow_with_All.py
+++ TensorFlow_with_All.py
@@ -26,15 +26,20 @@ docker_train = sc.fit_transform(X_train)
 docker_test = sc.transform(X_test)
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-    return outputs
+def add_layer(inputs, in_size, out_size,n_layer, activation_function=None):
+     layer_name = 'layer%s' % n_layer
+     with tf.name_scope(layer_name):
+        with tf.name_scope('Weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+        with tf.name_scope('Biases'):    
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.2)
+        with tf.name_scope('Formula'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)
+        return outputs
 
 def compute_accuracy(v_xs, v_ys):
     global prediction
@@ -43,8 +48,9 @@ def compute_accuracy(v_xs, v_ys):
     correct_prediction = tf.equal(y_pre, v_ys)
     floats=tf.cast(correct_prediction, tf.float32)
     print("correct_prediction:"+str(floats))
-
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    with tf.name_scope('acc'):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('acc', accuracy)
     result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
     return result
 
@@ -64,13 +70,13 @@ xs = tf.placeholder(tf.float32, [None, 13]) # 28x28
 ys = tf.placeholder(tf.float32, [None,])
 
 # add hidden layer
-l1 = add_layer(xs, 13, 13, activation_function=tf.nn.relu6)
+l1 = add_layer(xs, 13, 6,n_layer=1, activation_function=tf.nn.relu)
 
 # add hidden layer
-l2 = add_layer(l1, 13, 10, activation_function=tf.nn.relu6)
+l2 = add_layer(l1, 6, 6,n_layer=1, activation_function=tf.nn.relu)
 
 # add output layer
-prediction = add_layer(l2, 10, 1,  activation_function=tf.nn.sigmoid)
+prediction = add_layer(l2, 6, 1,n_layer=1,  activation_function=tf.nn.sigmoid)
 
 # the error between prediction and real data
 
@@ -80,6 +86,10 @@ train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
 
 sess = tf.Session()
 
+merged = tf.summary.merge_all()
+
+writer = tf.summary.FileWriter("F:\\tensorboardLog\\", sess.graph)
+
 # important step
 # tf.initialize_all_variables() no long valid from
 # 2017-03-02 if using tensorflow >= 0.12
@@ -87,11 +97,22 @@ if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[
     init = tf.initialize_all_variables()
 else:
     init = tf.global_variables_initializer()
-sess.run(init)
 
-for i in range(1000):
-    batch_xs, batch_ys = next_batch(200,docker_train,y_train)
+
+
+
+# Later, launch the model, initialize the variables, do some work, save the
+# variables to disk.
+with tf.Session() as sess:
+  sess.run(init)
+  for i in range(1000):
+    batch_xs, batch_ys = next_batch(100,docker_train,y_train)
    #dataset.train.n
     sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
-    if i % 100 == 0:
-        print(compute_accuracy(docker_test, y_test))
+    if i % 50 == 0:
+       result = sess.run(merged,feed_dict={xs: docker_train, ys: y_train})
+       writer.add_summary(result, i)
+  # Save the variables to disk.
+
+
+
